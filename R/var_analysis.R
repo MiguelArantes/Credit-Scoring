805c5bb35ae6ad46fc54b66a6e2d45d97bd8b92d
Miguel Arantes <miguelarantes.as@gmail.com>
2017-10-08 19:24:34 +0100

    feat(var_analysis): Add pc analysis.

--- R/var_analysis.R
+++ R/var_analysis.R
@@ -15,3 +15,15 @@ sample.data <- function(dataset, perc_size = 0.75, seed){
 
   return(list(train = train, test = test))
 }
+
+select.pca <- function(dataset){
+  class_ds <- lapply(dataset, class)
+  
+  log.ds <- log(dataset[names(class_ds[class_ds == "numeric"])])
+  
+  ds.pca <- stats::prcomp(log.ds,
+                   center = TRUE,
+                   scale. = TRUE) 
+  
+  return(ds.pca)
+}
