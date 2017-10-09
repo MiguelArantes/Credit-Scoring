a194d8eea1f4781077f6758c6722ac19826e4bac
Miguel Arantes <miguelarantes.as@gmail.com>
2017-10-09 22:32:59 +0100

    Add util function to separate train from test.

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,15 @@
-random.seed.gen <- function(n = 1, min = 0, max = 10^5){
-  seed <- sample(min:max, n, replace=TRUE)
-
+random.seed.gen <- function(n = 1, min = 0, max = 10 ^ 5) {
+  seed <- sample(min:max, n, replace = TRUE)
+  
   return(seed)
+}
+
+set.dataset <- function(dataset, type) {
+  if (is.list(dataset) &&
+      length(dataset) == 2 &&
+      as.logical(min(c("train", "test") %in% names(dataset)))) {
+    dataset <- dataset[type]
+  }
+  
+  return(dataset)
 }
--- R/var_analysis.R
+++ R/var_analysis.R
@@ -1,3 +1,5 @@
+source("utils.R")
+
 sample.data <- function(dataset, perc_size = 0.75, seed){
   if(missing(seed)){
   seed  <- random.seed.gen()
