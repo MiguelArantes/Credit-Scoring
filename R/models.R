ce237b6851eb9acead42ff221d8daadb8d03c7f5
Maikye <miguelarantes.as@gmail.com>
2017-11-14 16:42:33 +0000

    Add clustering analysis.

--- R/models.R
+++ R/models.R
@@ -66,6 +66,9 @@ set.clusters <- function(dataset, min_nc = 5, max_nc = 14){
                 index = "all", alphaBeale = 0.1)
   
 
-  nb <- mode.R(number_of_clusters$Best.nc[1,1:6])
-  return(nb)
+  nc <- mode.R(nc$Best.nc[1,1:6])
+  
+  ic <- kmeans(prediction[, list(prediction)],nb)
+  
+  return(list(number_of_clusters = nc, cluster_prediction = ic))
 }
